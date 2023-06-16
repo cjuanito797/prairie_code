c4f3c4db27c1b34a6288f4d20724073da06cbd37
Juan <jlizarraga@unomaha.edu>
2023-06-16 15:21:13 -0500

    c

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -1,6 +1,20 @@
 function changeBg() {
   var scrollValue = window.scrollY;
-  console.log(scrollValue);
+
+  var navbar = document.getElementById('nav_brand');
+  var navbar2 = document.getElementById('navbar_main');
+
+  if (scrollValue < 150) {
+    navbar.classList.remove('bgColor');
+    navbar2.classList.remove('bgColor');
+
+  }
+
+  else {
+    navbar.classList.add('bgColor');
+    navbar2.classList.add('bgColor');
+  }
+
 }
 
 
