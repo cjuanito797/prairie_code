822c79177b1662115f8f6eb7ad4f6b10f9fed3bd
Juan <jlizarraga@unomaha.edu>
2023-06-29 15:54:51 -0500

    added a tasks.py and updated dates file.

---
+++ home/tasks.py
@@ -0,0 +1,44 @@
+from celery import shared_task
+from datetime import datetime, timedelta, date
+
+@shared_task
+def slidingWindow():
+    today = date.today()
+    print("Running the scheduled sliding window task!")
+    myDates = []
+    f = open("dates.txt", "r")
+
+    for x in f:
+        myDates.append(x)
+    f.close()
+
+    print(myDates[0])
+    print(str(today) + "\n")
+    if (str (myDates[0]) == str (today) + '\n'):
+        # nothing to do here.
+        print("Nothing to do here moving on.")
+        myDates.clear ( )
+    else:
+        print("Moving the window one spot to the right.")
+        # append to list but first remove first value from list and get the next day that should be in our sliding window.
+
+        # delete yesterday's calendar entry.
+        # convert myDates[0] (yesterdays date into actual date)
+        yesterday = datetime.strptime (myDates[0].strip ('\n'), "%Y-%m-%d").date ( )
+
+        myDates.pop (0)
+        today = date.today()
+        nextDayInWindow = today + timedelta (days=30)
+
+        myDates.append (str (nextDayInWindow) + '\n')
+
+        # so with our new myDates file we can write them to our file.
+
+        f = open ("dates.txt", "w")
+
+        for day in myDates:
+            f.write (day)
+
+        f.close ( )
+
+        myDates.clear()
